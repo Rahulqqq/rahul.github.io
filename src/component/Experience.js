8eba36b3863bf7b2265397e85e96d08cc3edf0bf
Rahul Sharma <pt.rahulsharma007@gmail.com>
2024-11-11 12:49:48 +0530

    added links

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -1,135 +1,133 @@
 export function Experience() {
-    return (
-        <section className='card neumorphism-card-big'>
-        <h1 className='title'>Education</h1>
-        <div className='timeline-wrapper'>
-          <ul className='timeline'>
-            <li>
-              <div className='direction-r'>
-                <div className='flag-wrapper'>
-                  <span className='hexagon'></span>
-                  <span className='flag neumorphism-button' data-aos="zoom-in-left">
-                    <a href="" target="_blank" rel="noreferrer">
-                      <span>Full Stack Developer</span>
-                    </a>
-                  </span>
-                  {/* <span className='time-wrapper' data-aos="zoom-in-left">
-                    <span className='time neumorphism-card'>Aug 2021 - Present</span>
-                  </span> */}
+  return (
+      <section className='card neumorphism-card-big'>
+      <h1 className='title'>Education</h1>
+      <div className='timeline-wrapper'>
+        <ul className='timeline'>
+          <li>
+            <div className='direction-r'>
+              <div className='flag-wrapper'>
+                <span className='hexagon'></span>
+                <span className='flag neumorphism-button' data-aos="zoom-in-left">
+                  <a href="" target="_blank" rel="noreferrer">
+                    <span>Full Stack Developer</span>
+                  </a>
+                </span>
+                {/* <span className='time-wrapper' data-aos="zoom-in-left">
+                  <span className='time neumorphism-card'>Aug 2021 - Present</span>
+                </span> */}
+              </div>
+              <div className='neumorphism-card-mid'>
+                Full Stack Developer
+                <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                  Framework & Languages
+                    <div className="tag-wrapper">
+                        <span className="tag neumorphism-card">React</span>
+                        <span className="tag neumorphism-card">Node</span>
+                        <span className="tag neumorphism-card">Express</span>
+                        <span className="tag neumorphism-card">JavaScript </span>
+                        <span className="tag neumorphism-card">Python</span>
+                        <span className="tag neumorphism-card">DOM</span>
+                        <span className="tag neumorphism-card">jQuery</span>
+                        <span className="tag neumorphism-card">Unix</span>
+                        <span className="tag neumorphism-card">APIs</span>
+                        <span className="tag neumorphism-card">Git & Github</span>
+                        <span className="tag neumorphism-card">MongoDB</span>
+                        <span className="tag neumorphism-card">MySQL</span>
+                        <span className="tag neumorphism-card">Mongoose</span>
+                        <span className="tag neumorphism-card">Authentication & Security</span>
+                    </div>
                 </div>
-                <div className='neumorphism-card-mid'>
-                  Full Stack Developer
-                  <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                    Framework & Languages
-                      <div className="tag-wrapper">
-                          <span className="tag neumorphism-card">React</span>
-                          <span className="tag neumorphism-card">Node</span>
-                          <span className="tag neumorphism-card">Express</span>
-                          <span className="tag neumorphism-card">JavaScript </span>
-                          <span className="tag neumorphism-card">Python</span>
-                          <span className="tag neumorphism-card">DOM</span>
-                          <span className="tag neumorphism-card">jQuery</span>
-                          <span className="tag neumorphism-card">Unix</span>
-                          <span className="tag neumorphism-card">APIs</span>
-                          <span className="tag neumorphism-card">Git & Github</span>
-                          <span className="tag neumorphism-card">MongoDB</span>
-                          <span className="tag neumorphism-card">MySQL</span>
-                          <span className="tag neumorphism-card">Mongoose</span>
-                          <span className="tag neumorphism-card">Authentication & Security</span>
-                      </div>
-                  </div>
-                  <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                    IDEs & Tools
-                      <div className="tag-wrapper">
-                          <span className="tag neumorphism-card">Visual Studio Code</span>
-                          
-                          <span className="tag neumorphism-card">Intellij IDEA</span>
-                          <span className="tag neumorphism-card">Postman</span>
-                          {/* <span className="tag neumorphism-card">Splunk</span>
-                          <span className="tag neumorphism-card">Jira</span>
-                          <span className="tag neumorphism-card">Bitbucket</span>
-                          <span className="tag neumorphism-card">Bamboo</span> */}
-                          <span className="tag neumorphism-card">AWS</span>
-                      </div>
-                  </div>
+                <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                  IDEs & Tools
+                    <div className="tag-wrapper">
+                        <span className="tag neumorphism-card">Visual Studio Code</span>
+                        
+                        <span className="tag neumorphism-card">Intellij IDEA</span>
+                        <span className="tag neumorphism-card">Postman</span>
+                        {/* <span className="tag neumorphism-card">Splunk</span>
+                        <span className="tag neumorphism-card">Jira</span>
+                        <span className="tag neumorphism-card">Bitbucket</span>
+                        <span className="tag neumorphism-card">Bamboo</span> */}
+                        <span className="tag neumorphism-card">AWS</span>
+                    </div>
                 </div>
               </div>
-            </li>
-            <li>
-              <div className='direction-l'>
-                <div className='flag-wrapper'>
-                  <span className='hexagon'></span>
-                  <span className='flag neumorphism-button' data-aos="zoom-in-left">
-                    <a href="https://bce.ac.in/" target="_blank" rel="noreferrer">
-                      <span>Bansal College of Engineering, Mandideep</span>
-                    </a>
-                  </span>
-                  <span className='time-wrapper' data-aos="zoom-in-left">
-                    <span className='time neumorphism-card'>July 2017 - July 2021</span>
-                  </span>
-                </div>
-                <div className='neumorphism-card-mid'>
-                Bachelor of Technology
-                  <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                      Computer Science & Engineering
-                      <div className="tag-wrapper">
-                          <span className="tag neumorphism-card">C</span>
-                          <span className="tag neumorphism-card">C++</span>
-                          <span className="tag neumorphism-card">Python</span>
-                          <span className="tag neumorphism-card">HTML</span>
-                          <span className="tag neumorphism-card">CSS</span>
-                          <span className="tag neumorphism-card">Bootstrap</span>
-                          <span className="tag neumorphism-card">JavaScript</span>
-                          <span className="tag neumorphism-card">Data Structure</span>
-                          <span className="tag neumorphism-card">Object Oriented Programming</span>
-                          <span className="tag neumorphism-card">Software Engineering</span>
-                          <span className="tag neumorphism-card">Communication</span>
-                          <span className="tag neumorphism-card">Problem Solving</span>
-                          <span className="tag neumorphism-card">Visual Studio Code</span>
-                      </div>
-                  </div>
+            </div>
+          </li>
+          <li>
+            <div className='direction-l'>
+              <div className='flag-wrapper'>
+                <span className='hexagon'></span>
+                <span className='flag neumorphism-button' data-aos="zoom-in-left">
+                  <a href="https://bce.ac.in/" target="_blank" rel="noreferrer">
+                    <span>Bansal College of Engineering, Mandideep</span>
+                  </a>
+                </span>
+                <span className='time-wrapper' data-aos="zoom-in-left">
+                  <span className='time neumorphism-card'>July 2017 - July 2021</span>
+                </span>
+              </div>
+              <div className='neumorphism-card-mid'>
+              Bachelor of Technology
+                <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                    Computer Science & Engineering
+                    <div className="tag-wrapper">
+                        <span className="tag neumorphism-card">C</span>
+                        <span className="tag neumorphism-card">C++</span>
+                        <span className="tag neumorphism-card">Python</span>
+                        <span className="tag neumorphism-card">HTML</span>
+                        <span className="tag neumorphism-card">CSS</span>
+                        <span className="tag neumorphism-card">Bootstrap</span>
+                        <span className="tag neumorphism-card">JavaScript</span>
+                        <span className="tag neumorphism-card">Data Structure</span>
+                        <span className="tag neumorphism-card">Object Oriented Programming</span>
+                        <span className="tag neumorphism-card">Software Engineering</span>
+                        <span className="tag neumorphism-card">Communication</span>
+                        <span className="tag neumorphism-card">Problem Solving</span>
+                        <span className="tag neumorphism-card">Visual Studio Code</span>
+                    </div>
                 </div>
               </div>
-            </li>
-            <li>
-              <div className='direction-r'>
-                <div className='flag-wrapper'>
-                  <span className='hexagon'></span>
-                  <span className='flag neumorphism-button' data-aos="zoom-in-left">
-                    <a href="" target="_blank" rel="noreferrer">
-                      <span>Oriental College of Technology, Bhopal</span>
-                    </a>
-                  </span>
-                  <span className='time-wrapper' data-aos="zoom-in-left">
-                    <span className='time neumorphism-card'>Aug 2022 - Aug 2024</span>
-                  </span>
-                </div>
-                <div className='neumorphism-card-mid'>
-                  Master of Technology
-                  <div className="desc neumorphism-card" data-aos="zoom-in-left">
-                   Computer Science & Engineering
-                      <div className="tag-wrapper">
-                          <span className="tag neumorphism-card">Machine Learning</span>
-                          <span className="tag neumorphism-card">Python</span>
-                          <span className="tag neumorphism-card">Java</span>
-                          <span className="tag neumorphism-card">Spring-Boot</span>
-                          <span className="tag neumorphism-card">PostMan</span>
-                          <span className="tag neumorphism-card">Linux</span>
-                          <span className="tag neumorphism-card">APIs</span>
-                          <span className="tag neumorphism-card">Git & Github</span>
-                          <span className="tag neumorphism-card">MongoDB</span>
-                          <span className="tag neumorphism-card">MySQL</span>
-                          <span className="tag neumorphism-card">Mongoose</span>
-                          <span className="tag neumorphism-card">Authentication & Security</span>
-                      </div>
-                  </div>
+            </div>
+          </li>
+          <li>
+            <div className='direction-r'>
+              <div className='flag-wrapper'>
+                <span className='hexagon'></span>
+                <span className='flag neumorphism-button' data-aos="zoom-in-left">
+                  <a href="" target="_blank" rel="noreferrer">
+                    <span>Oriental College of Technology, Bhopal</span>
+                  </a>
+                </span>
+                <span className='time-wrapper' data-aos="zoom-in-left">
+                  <span className='time neumorphism-card'>Aug 2022 - Aug 2024</span>
+                </span>
+              </div>
+              <div className='neumorphism-card-mid'>
+                Master of Technology
+                <div className="desc neumorphism-card" data-aos="zoom-in-left">
+                 Computer Science & Engineering
+                    <div className="tag-wrapper">
+                        <span className="tag neumorphism-card">Machine Learning</span>
+                        <span className="tag neumorphism-card">Python</span>
+                        <span className="tag neumorphism-card">Java</span>
+                        <span className="tag neumorphism-card">Spring-Boot</span>
+                        <span className="tag neumorphism-card">PostMan</span>
+                        <span className="tag neumorphism-card">Linux</span>
+                        <span className="tag neumorphism-card">APIs</span>
+                        <span className="tag neumorphism-card">Git & Github</span>
+                        <span className="tag neumorphism-card">MongoDB</span>
+                        <span className="tag neumorphism-card">MySQL</span>
+                        <span className="tag neumorphism-card">Mongoose</span>
+                        <span className="tag neumorphism-card">Authentication & Security</span>
+                    </div>
                 </div>
               </div>
-            </li>
-          </ul>
-        </div>
-      </section>
-    )
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
-
-
