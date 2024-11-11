e7a7556706cfa9f4ad228b7c8069b76b49637fb6
Rahul Sharma <pt.rahulsharma007@gmail.com>
2024-11-11 12:47:17 +0530

    disabled lint unused vars warning

--- src/component/styled/Sdiv.js
+++ src/component/styled/Sdiv.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import styled, { keyframes } from 'styled-components';
 import { merge, bounce, fadeInDown, fadeIn, zoomIn, flash, flipInX } from 'react-animations';
 
@@ -13,3 +14,4 @@ const FadeInXZoomIn = styled.div.attrs({})` animation: 2s ${keyframes `${fadeInX
 export const Flash = styled.h1.attrs({})` animation: 5s ${keyframes `${flash}`}`;
 
 export default FlipInX;
+/* eslint-disable no-unused-vars */
